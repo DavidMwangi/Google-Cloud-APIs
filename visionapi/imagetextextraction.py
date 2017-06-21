c5104dc6b0edb200a61e43f6a7bfd840bcc527b5
DavidMwangi <david.mathenge98@gmail.com>
2017-06-21 16:45:07 +0300

    Text Extraction from Image (OCR)

---
+++ visionapi/imagetextextraction.py
@@ -0,0 +1,15 @@
+import io
+
+from google.cloud import vision
+
+vision_client = vision.Client()
+
+file_name = 'block-of-text.png'
+
+with io.open(file_name, 'rb') as image_file:
+
+    content = image_file.read()
+
+    image = vision_client.image(content = content)
+
+print(image.detect_full_text().text)
